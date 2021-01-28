4b16945a2a94b20f2e573921090e8d4eeb2e74b0
cuspencer <cuspencer@gmail.com>
2021-01-28 16:03:19 +0100

    Added sysinfo to footer.

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
 
 <div id="footer" class="w3-container w3-khaki">
-  <h5 style="center"> DATE: <?php echo date('D, d M Y');?></h5>
+  <h5 style="center">
+    <?php
+      echo date('D, d M Y');
+      echo " SYSTEM: " . $_SESSION['sysName'];
+
+    ?>
+  </h5>
 </div>
 
 </html>
